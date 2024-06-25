2ae0baef1d772aef1aea4ebb7000c89877c6d7d1
Adamnson-0dinson <theawakeavatar@gmail.com>
2024-06-25 15:59:30 +0530

    handle repeated letter replacement
    -> a function "check_and_replace" was implemented
    to check the number of occurrences of the guess
    and replace all occurrences if found
    
    -> the function takes in the target word, the
    dispaly string and the guess as the input
    parameters

--- hangman.rb
+++ hangman.rb
@@ -2,18 +2,31 @@ contents = File.readlines('google-10000-english-no-swears.txt')
 
 words = contents.filter { |word| word.length.between?(6,13)}
 
-target = words.sample
+target = words.sample.chop    # chop the trailing "\n"
 display = ""
+
+def check_and_replace(target, display, guess)
+  if target.include?(guess)
+    if target.count(guess) == 1
+      display[target.index(guess)] = guess   # will replace only the first occurance
+    else
+      target.chars.each_with_index  do |ch, idx|
+        if ch.eql?(guess)
+          display[idx] = guess
+        end
+      end
+    end
+  end
+end
+
 puts target
-(target.size - 1).times do    # (-1) because the last character is \n
+target.size.times do
   display += "*"
 end
 puts display
-puts "\nGuess a letter : "
+print "\nGuess a letter : "
 guess = gets.chomp
 print "You inputs #{guess}\n"
-if target.include?(guess)    # this will replace only the first occurance
-  display.g guess
-end
+check_and_replace(target, display, guess)
 puts display
 
