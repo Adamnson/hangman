2826e8b0f192f5e2f230071b61cb3c62ec7980db
Adamnson-0dinson <theawakeavatar@gmail.com>
2024-06-28 07:37:22 +0530

    implement game loop
    defined a run method that runs the game in a loop
    until the chances run out (tracked through the variable
    @rounds)  or the word is correctly guessed

--- hangman.rb
+++ hangman.rb
@@ -1,18 +1,12 @@
-contents = File.readlines('google-10000-english-no-swears.txt')
-
-words = contents.filter { |word| word.length.between?(6,13)}
-
-target = words.sample.chop    # chop the trailing "\n"
-display = ""
-
 class HangmanGame 
-  attr_reader :display, :target
+  attr_reader :display, :target, :rounds
 
   def initialize
     contents = File.readlines('google-10000-english-no-swears.txt')
     words = contents.filter { |word| word.length.between?(6,13)}
     @target = words.sample.chop
     @display = "" 
+    @rounds = 7
     @target.size.times do
       @display += "*"
     end
@@ -29,20 +23,31 @@ class HangmanGame
           end
         end
       end
+    else
+      @rounds -= 1
     end
   end # check_and_replace
+
+  def run
+    print @display + "\t"
+    while @rounds > 0 && display.include?("*")
+      # puts @target
+      puts @rounds
+      print "Guess a letter : "
+      guess = gets.chomp
+      print "You inputs #{guess}\n"
+      check_and_replace(guess)
+      print @display + "\t"
+    end
+    puts ""
+  end
 
 end #class HangmanGame
 
 
 game = HangmanGame.new
 
-puts game.target
+game.run
 
-puts game.display
-print "\nGuess a letter : "
-guess = gets.chomp
-print "You inputs #{guess}\n"
-game.check_and_replace(guess)
-puts game.display
+
 
